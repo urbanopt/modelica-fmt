2b6c3632871c4196507214ecdf8732363fa98972
Tomasz Wojdat <trivvz3@gmail.com>
2021-08-11 23:41:16 +0200

    Add `--rm` to `docker run` so antlr container leftovers are removed

--- generate_parser.sh
+++ generate_parser.sh
@@ -2,6 +2,6 @@
 
 docker build -t antlr4:latest -f thirdparty/Dockerfile-Antlr .
 
-docker run -v "$(pwd)/thirdparty":/var/antlrResult \
+docker run --rm -v "$(pwd)/thirdparty":/var/antlrResult \
         antlr4:latest \
         -Dlanguage=Go -o /var/antlrResult/parser /var/antlrResult/Modelica.g4
